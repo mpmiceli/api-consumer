4368ab9f3465c02da544e99ab96bb7766881d0cf
Maria Paula Miceli <mariapaulamiceli@gmail.com>
2018-06-17 21:47:12 -0300

    version javascript

--- general.js
+++ general.js
@@ -11,21 +11,23 @@ function capitalize(str1){
     return str1.charAt(0).toUpperCase() + str1.slice(1);
 }
 
-function createProfile(user) {
+function createProfile(user, index) {
     let divMain = document.createElement("div");
-    divMain.className = "col-xs-12 col-sm-6 col-md-3";
-    divMain.innerHTML = '<div class="thumbnail">' +
+        divMain.className = "col-xs-12 col-sm-6 col-md-3";
+        divMain.innerHTML = '<div class="thumbnail">' +
         '<a href="#"><img src="' + user.picture.large + '" alt="' + user.picture.thumbnail + '"></a>' +
         '<div class="caption">' + 
             '<h3>' + capitalize(user.name.first) + ' ' + capitalize(user.name.last) + '</h3>' +
             '<p>' + capitalize(user.location.street)  + '</p>' +
             '<p>' + capitalize(user.location.city) + '</p>' +
             '<p>' + capitalize(user.location.state) + '</p>' +
-            '<a href="detalle.html?id=15" class="btn btn-primary">Ver perfil completo</a>' +
+            '<a href="detalle.html?id=' + index + '" class="btn btn-primary">Ver perfil completo</a>' +
         '</div>' +
     '</div>';
     return divMain;
 }
+
+
 
 const container = document.getElementById('profileContainer');
 const url = 'https://randomuser.me/api/?results=10';
@@ -33,31 +35,15 @@ fetch(url)
   .then((resp) => resp.json())
   .then(function(data) {
     let users = data.results;
-    sessionStorage.setItem('users', JSON.stringify(users));
-    return users.map(function(user) {
-        let profile = createProfile(user);
+    localStorage.setItem('users', JSON.stringify(users));
+    return users.map(function(user, index) {
+        let profile = createProfile(user, index);
         container.appendChild(profile);  
     })
   })
   .catch(function(error) {
     console.log(error);
   }); 
-
-/*const ul = document.getElementById('authors');
-  const url = 'https://randomuser.me/api/?results=10';
-  fetch(url)
-  .then((resp) => resp.json())
-  .then(function(data) {
-    let authors = data.results;
-    return authors.map(function(author) {
-      let img = author.picture.medium;
-        $name = author.name.first;
-        $last = author.name.last;
-    })
-  })
-  .catch(function(error) {
-    console.log(error);
-  });  */ 
 
     
 
--- prueba.php
+++ prueba.php
@@ -47,150 +47,74 @@ echo " ";*/
                 <div class="navbar-header">
                     <h2 style="color:#fff; font-size: 30px;">Perfiles</h2>
 
-                    <!--<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#btn-colapsar">
-                        <span class="sr-only">Navegacion</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+                   
                 </div>-->
 
-                <!--ELEMENTOS DEL MENU-->
-         <!-- <div class="collapse navbar-collapse" id="btn-colapsar">
-                    <ul class="nav navbar-nav">
-                        <li><a href="#">Inicio</a></li>-->
-                       <!--<li>
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                Productos <span class="caret"></span>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Computadoras</a></li>
-                                    <li><a href="#">Laptops</a></li>
-                                    <li><a href="#">Smartphones</a></li>
-                                    <li><a href="#">Drones</a></li>
-                                    <li><a href="#">Accesorios</a></li>
-                                </ul>
+                <!--Galeria-->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="page-header">
+                            <h2 style="color: #fff; font-size: 30px; text-align: center;">Galeria</h2>
+                        </div>
+                    </div>
+                    <?php for ($posicion = 0; $posicion <16; $posicion++){ ?>
+                    <div class="col-xs-12 col-sm-6 col-md-3">
+                        <div class="thumbnail">
+                            <a href="#">
+                                <img src="<?php echo $response->results[$posicion]->picture->large; ?>" alt="<?php echo $response->results[0]->picture->thumbnail; ?>">
                             </a>
-                       </li>
-                        <li><a href="#">Contacto</a></li>
-                    </ul>-->
-
-                    <!--Formulario de Busqueda-->
-                    <!--<form action="" class="navbar-form navbar-right">
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Buscar">
-                            <button type="submit" class="btn btn-default">Buscar</button>
-                        </div>
-                    </form>
-                </div>
-
-            </div>
-        </nav>
-    </header>
-
-    <section class="main">
-        <div class="conteiner">
-            <!--Slideshow-->
-            <!--<div class="row">
-                <div class="col-md-12">
-                   <div id="slider" class="carousel slide" data-ride="carousel">
-                        <!--Indicadores-->
-                       <!--<ol class="carousel-indicators">
-                            <li data-target="#slider" data-slide-to="0" class="active"></li>
-                            <li data-target="#slider" data-slide-to="1"></li>
-                            <li data-target="#slider" data-slide-to="2"></li>
-                        </ol>
-
-                        <!--Contenedor de Slides-->
-                        <!--<div class="carousel-inner">
-                            <div class="item active">
-                                <img src="img/s1.jpg" alt="">
-                            </div>
-                            <div class="item">
-                                <img src="img/s2.jpg" alt="">
-                            </div>
-                            <div class="item">
-                                <img src="img/s3.jpg" alt="">
-                            </div>
-                        </div>
-
-                        <!--Controles-->
-                        <!--<a href="#slider" class="left carousel-control" data-slide="prev">
-                            <span class="glyphicon glyphicon-chevron-left"></span>
-                            <span class="sr-only">Anterior</span>
-                        </a>
-                        <a href="#slider" class="right carousel-control" data-slide="next">
-                            <span class="glyphicon glyphicon-chevron-right"></span>
-                            <span class="sr-only">Siguiente</span>
-                        </a>
-                   </div> 
-                </div>
-            </div>-->
-            <!--Galeria-->
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="page-header">
-                        <h2 style="color: #fff; font-size: 30px; text-align: center;">Galeria</h2>
-                    </div>
-                </div>
-                <?php for ($posicion = 0; $posicion <16; $posicion++){ ?>
-                <div class="col-xs-12 col-sm-6 col-md-3">
-                    <div class="thumbnail">
-                        <a href="#">
-                            <img src="<?php echo $response->results[$posicion]->picture->large; ?>" alt="<?php echo $response->results[0]->picture->thumbnail; ?>">
-                        </a>
-                        <div class="caption">
-                            <h3>
-                                <?php
-                                    $str = $response->results[$posicion]->name->first;
+                            <div class="caption">
+                                <h3>
+                                    <?php
+                                        $str = $response->results[$posicion]->name->first;
+                                        $str = ucfirst($str);
+                                        echo $str;
+                                        echo " ";
+                                        $str = $response->results[$posicion]->name->last;
+                                        $str = ucfirst($str);
+                                        echo $str; 
+                                    ?>
+                                </h3>
+                                <p>
+                                    <?php 
+                                        $str = $response->results[$posicion]->location->street;
+                                        $str = ucwords($str);
+                                        $str = substr($str, 0, 30);
+                                        echo $str;
+                                    ?>
+                                </p>
+                                <p>
+                                    <?php 
+                                    $str = $response->results[$posicion]->location->city;
                                     $str = ucfirst($str);
-                                    echo $str;
-                                    echo " ";
-                                    $str = $response->results[$posicion]->name->last;
-                                    $str = ucfirst($str);
-                                    echo $str; 
-                                ?>
-                            </h3>
-                            <p>
-                                <?php 
-                                    $str = $response->results[$posicion]->location->street;
-                                    $str = ucwords($str);
-                                    $str = substr($str, 0, 30);
-                                    echo $str;
-                                ?>
-                            </p>
-                            <p>
-                                <?php 
-                                $str = $response->results[$posicion]->location->city;
-                                $str = ucfirst($str);
-                                echo substr($str, 0, 30);
-                                ?>
-                            </p>
-                            <p><?php 
-                                    $str = $response->results[$posicion]->location->state;
-                                    $str = ucfirst($str); 
-                                    echo $str; 
-                                ?>
-                            </p>
-                            <a href="http://api-consumer.local/detalle.php?posicion=15" class="btn btn-primary">Ver perfil completo</a>
+                                    echo substr($str, 0, 30);
+                                    ?>
+                                </p>
+                                <p><?php 
+                                        $str = $response->results[$posicion]->location->state;
+                                        $str = ucfirst($str); 
+                                        echo $str; 
+                                    ?>
+                                </p>
+                                <a href="http://api-consumer.local/detalle.php?posicion=15" class="btn btn-primary">Ver perfil completo</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <?php } ?>
+                    <?php } ?>
                 
 
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <hr>
+                    <footer>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <hr>
+                                </div>  
+                            </div>
+                        </div>
+                    </footer>
 
-                </div>  
-            </div>
-        </div>
-    </footer>
-
-    <script src="base/js/jquery.js"></script>
-    <script src="base/js/bootstrap.min.js"></script>
+        <script src="base/js/jquery.js"></script>
+        <script src="base/js/bootstrap.min.js"></script>
 </body>
 </html>
 
